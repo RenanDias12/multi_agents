6adf1bf9285af6c8a195c61e96c19a60d1eae803
Renan Dias <renandias@gec.inatel.br>
2025-03-11 22:10:00 -0300

    add env template

--- src/agent.ts
+++ src/agent.ts
@@ -1,26 +1,18 @@
 import 'dotenv/config';
-import * as fs from 'fs';
-import * as path from 'path';
 
-import { ChatGroq } from '@langchain/groq';
 import { MemorySaver } from "@langchain/langgraph";
 import { HumanMessage } from "@langchain/core/messages";
 import { ResearcherAgent } from './agents/researcher.js';
 
-// Define the tools for the agent to use
-const agentModel = new ChatGroq({
-    apiKey: process.env.GROQ_API_KEY,
-    modelName: 'llama-3.2-3b-preview',
-});
 
 // Initialize memory to persist state between graph runs
 const agentCheckpointer = new MemorySaver();
 const researcher = new ResearcherAgent();
-// const msg = new HumanMessage('Qual a ultima versão do Node.js?');
-// const response = await researcher.invoke(
-// { messages: [msg] },
-// { checkpointSaver: agentCheckpointer }
-// );
-// console.log(response)
+const msg = new HumanMessage('Qual a ultima versão do Node.js?');
+const response = await researcher.invoke(
+    { messages: [msg] },
+    { checkpointSaver: agentCheckpointer }
+);
+console.log(response)
 
-await researcher.drawGraph()
+// await researcher.drawGraph()
